bab02663a112490683a207c02a4c1254b05e399c
StasyHard <reyngardt.as@yandex.ru>
2020-07-28 11:12:36 +0300

    first commit

---
+++ PryanikyTest/Screens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PryanikyTest
+//
+//  Created by Anastasia Reyngardt on 28.07.2020.
+//  Copyright © 2020 GermanyHome. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
